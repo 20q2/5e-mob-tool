95675086bc8a7a0b2028fae0a69b3ab8896e2241
Andrew Brogan <andrewnintendo@msn.com>
2020-06-29 17:39:44 -0400

    Add files via upload
    Added a version compatible with 3.8

---
+++ Necromancer-Tool 3.8.py
@@ -0,0 +1,32 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: F:\Coding\Python\Necromancer-Tool.py ================
+Please give the number of skellys 💀
+10
+You have selected 10 to be your size
+Please give the number of zombs 🧟️
+5
+You have selected 5 to be your size
+Please give the number of ghouls 👤️
+1
+You have selected 1 to be your size
+Please input target 🛡AC:
+10
+Target 🛡AC is 10
+12 attacks landed! (1 crits)
+Dealt 132 damage!
+ -- == -- == -- == -- == --
+💀 ⚔7 [14] Light Crossbow: +4 to hit, (1d8) + 6 piercing damage
+💀 ⚔15 [crit] Light Crossbow: +4 to hit, (1d8) + 6 piercing damage 🌟CRIT!
+💀 ⚔12 [21] Light Crossbow: +4 to hit, (1d8) + 6 piercing damage
+💀 ⚔13 [14] Light Crossbow: +4 to hit, (1d8) + 6 piercing damage
+💀 ⚔11 [18] Light Crossbow: +4 to hit, (1d8) + 6 piercing damage
+💀 ⚔13 [22] Light Crossbow: +4 to hit, (1d8) + 6 piercing damage
+💀 ⚔9 [18] Light Crossbow: +4 to hit, (1d8) + 6 piercing damage
+💀 ⚔10 [11] Light Crossbow: +4 to hit, (1d8) + 6 piercing damage
+💀 ⚔13 [18] Light Crossbow: +4 to hit, (1d8) + 6 piercing damage
+🧟 ⚔11 [19] Slam: +3 to hit, (1d6) + 5 bludgeoning damage
+🧟 ⚔10 [13] Slam: +3 to hit, (1d6) + 5 bludgeoning damage
+🧟 ⚔8 [15] Slam: +3 to hit, (1d6) + 5 bludgeoning damage
+>>> 
